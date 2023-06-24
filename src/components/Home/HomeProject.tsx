5ff9f750e357648bf826bbecdb54d7eb5fdcda54
Jaedan Persaud <63131031+jaedanpersaud12@users.noreply.github.com>
2023-06-24 15:49:31 -0400

    banner

--- src/components/Home/HomeProject.tsx
+++ src/components/Home/HomeProject.tsx
@@ -20,7 +20,7 @@ const HomeProject = () => {
       className="relative w-full min-h-screen px-20 py-10 mx-auto"
     >
       <Link href="https://safia.vercel.app/">
-        <div className="rounded-xl bg-text max-w-[1000px] h-[600px] flex items-center justify-center">
+        <div className="rounded-xl bg-accent max-w-[1000px] h-[600px] flex items-center justify-center">
           <SafiaBanner />
         </div>
       </Link>
