7d515a959e5a3b997d38abda4a92536ffc4de0db
Jaedan Persaud <63131031+jaedanpersaud12@users.noreply.github.com>
2023-06-23 21:47:03 -0400

    Update TypeAnimation.tsx

--- src/components/atoms/TypeAnimation.tsx
+++ src/components/atoms/TypeAnimation.tsx
@@ -8,17 +8,24 @@ type Props = {};
 const TypeAnimationComponent = (props: Props) => {
   return (
     <TypeAnimation
-      className="absolute bottom-0 left-0 p-10 uppercase text-text"
+      className="absolute bottom-0 left-0 p-10 font-semibold uppercase text-text"
       sequence={[
         "IN PROGRESS...",
         1000,
+
         "I STILL HAVE A LONG WAY TO GO...",
         1000,
         "MAYBE I'LL DO SOME PARALLAX STUFF...",
         1000,
         "OR SOME WACKY ANIMATIONS...",
         1000,
+        "YOU SHOULD CHECK OUT THE DARK/LIGHT MODES",
+        1000,
         "LET ME KNOW WHAT YOU THINK",
+        1000,
+        "OBSESSED WITH THAT TITLE ANIMATION...",
+        1000,
+        "AND THE NAVIGATION ANIMATION...",
         1000,
       ]}
       wrapper="span"
